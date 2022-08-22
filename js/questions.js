85df280274182daf7b00160f2c0d11c9da0236e3
hen <hendrapart2002@gmail.com>
2022-08-22 21:24:48 +0700

    questions default

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Candi Borubur merupakan candi agama?",
+    answer: "Buddha",
+    options: [
+      "Hindhu",
+      "Katholik",
+      "Kristen",
+      "Buddha"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Suku Aborigin berasal dari negara?",
+    answer: "Australia",
+    options: [
+      "Australia",
+      "Amerika",
+      "Eropa",
+      "India"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Mata uang negara Jepang?",
+    answer: "Yen",
+    options: [
+      "Yuan",
+      "Yen",
+      "Ringgit",
+      "Lira"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Julukan kota Pelajar di Indonesia adalah?",
+    answer: "Yogyakarta",
+    options: [
+      "Palembang",
+      "Solo",
+      "Yogyakarta",
+      "Bandung"
+    ]
+  },
+    {
+    numb: 5,
+    question: "kepanjangan dari WHO adalah?",
+    answer: "World Health Organization",
+    options: [
+      "World High Organization",
+      "Where Health Organization",
+      "World Health Opposition",
+      "World Health Organization"
+    ]
+  },
+];
